5800067c3e135b5b8bf35b8bc5cfd9bf1c3bad7c
Johannes Liermann <liermann@uni-mainz.de>
2026-03-02 18:50:51 +0100

    feat: implement FeatureDial component and integrate with Features

---
+++ src/components/features/FeatureDial.js
@@ -0,0 +1,44 @@
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import Link from "@docusaurus/Link";
+
+import styles from "@site/src/css/Features.module.css";
+
+import clsx from "clsx";
+
+function FeatureDial({ url, imgUrl, text, dial, ...props }) {
+  let classes = clsx(
+    "button",
+    { "button--primary": props.index },
+    { "button--secondary": !props.index },
+    props.classes,
+    styles.featureButton,
+    { [styles.featureButtonIndex]: props.index },
+  );
+
+  const width = props?.width ?? "120px";
+
+  return (
+    <div className={styles.featureDial}>
+      <Link to={url} className={classes}>
+        <div className={styles.featureSvg}>
+          <img src={useBaseUrl(imgUrl)} style={{ width: width }} alt={props.alt ?? text} />
+        </div>
+        <div>{text}</div>
+      </Link>
+      <div className={styles.featureDialContent}>
+        {dial.map((item, idx) => (
+          <div key={idx} className={styles.featureDialItem}>
+            <div>
+              <img src={useBaseUrl(item.imgUrl)} style={{ width: "40px" }} alt={item.alt ?? item.text} />
+            </div>
+            <div>
+              {item.text}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default FeatureDial;
--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -1,20 +1,32 @@
 import ButtonContainer from "./ButtonContainer";
 import FeatureButton from "./FeatureButton";
+import FeatureDial from "./FeatureDial";
 
 function Features({ featureList, index, ...props }) {
   return (
     <ButtonContainer>
       {featureList.map((feature, idx) => (
-        <FeatureButton
-          key={idx}
-          url={feature.url}
-          imgUrl={feature.imgUrl}
-          text={feature.text}
-          alt={feature.alt}
-          index={index}
-          {...props}
-        />
-      ))}
+        feature.dial ? (
+          <FeatureDial
+            key={idx}
+            url={feature.url}
+            imgUrl={feature.imgUrl}
+            text={feature.text}
+            alt={feature.alt}
+            index={index}
+            dial={feature.dial}
+            {...props}
+          />) : (
+          <FeatureButton
+            key={idx}
+            url={feature.url}
+            imgUrl={feature.imgUrl}
+            text={feature.text}
+            alt={feature.alt}
+            index={index}
+            {...props}
+          />
+        )))}
     </ButtonContainer>
   );
 }
